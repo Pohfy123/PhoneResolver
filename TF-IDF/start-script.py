283cf8b05a0cc820f41b013ea043668ed3661f76
Roos <Vasvaroos@gmail.com>
2016-10-03 15:45:18 +0700

    move file and add print remark each step

--- TF-IDF/start-script.py
+++ TF-IDF/start-script.py
@@ -15,12 +15,16 @@ feature_mode = 1       # TF
 # feature_mode = 2    # TF-IDF
 
 # STEP1: Google Crawling
+print "\n\n############# RUN STEP1: Google Crawling #############"
 
 # STEP2: Thai Parser (PyICU)
+print "\n\n############# RUN STEP2: Thai Parser (PyICU) #############"
 wordParser_PyICU.parseAllDocuments(path_raw_data, path_parsed_words, delimeter)
 
 # STEP3: N-Gram
+print "\n\n############# RUN STEP3: N-Gram #############"
 ngrams.applyNgramAllDocuments(path_parsed_words, path_ngram_words, number_of_gram, delimeter)
 
 # STEP4: Extract Feature (TF / TF-IDF)
+print "\n\n############# RUN STEP4: Extract Feature (TF / TF-IDF) #############"
 extract_feature.extract_feature(path_ngram_words, path_result, feature_mode)
