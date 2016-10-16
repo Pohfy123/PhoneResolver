07e465e7c4eb64dd8bd2136c04b578b7febd8f5f
Pohfy123 <pohfy123@gmail.com>
2016-10-16 19:54:04 +0700

    fix  text classification

--- TF-IDF/classification_bone/textclassification.py
+++ TF-IDF/classification_bone/textclassification.py
@@ -6,7 +6,7 @@ sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf8', buffering=1)
 import PyICU
 import nltk
 import random
-
+o = open('./predict_result.txt', 'w')
 def isThai(chr):
     cVal = ord(chr)
     if (cVal >= 3584 and cVal <= 3711):
@@ -36,34 +36,36 @@ def warp(txt):
     return retTxt
 
 N = 2
-numberOfRows = 250
+numberOfRows = 124
+nRowTrain = 124
 dict = {}
 word_features_taste = [] #word_features_taste = ['รสชาติ','อร่อยมาก','มาก','สุด']
-f = open('./data/data250_1000.csv', newline='', encoding='utf-8')
+f = open('./data/result.csv', newline='', encoding='utf-8')
 segmentedArr = []
-reviewList = []
+topWordsList = []
 i = 0
 for line in f:
     if (i is numberOfRows):break
     i = i+1
-    id, review, n_taste, n_env, n_service = line.strip().split(',')
-    if n_service == '1': #TODO : EDIT
-        segmented = warp(review).split('|')
+    id, topWords, ans = line.strip().split(',')
+    if ans == '1': #TODO : EDIT
+        segmented = warp(topWords).split(' ')
         for i in range(0, len(segmented)):
             wordGrammed = ''
             for j in range(0, N):
                 if i + j in range(0, len(segmented)):
                     wordGrammed += segmented[i + j]
-            reviewList.append(wordGrammed)
+            topWordsList.append(wordGrammed)
 word_counter = {}
-for word in reviewList:
+for word in topWordsList:
     if word in word_counter:
         word_counter[word] += 1
     else:
         word_counter[word] = 1
 popular_words = sorted(word_counter, key = word_counter.get, reverse = True)
-word_features_taste = popular_words[:20]
-print(word_features_taste)
+word_features_taste = popular_words[:100]
+# print(word_features_taste)
+o.write(str('\n'.join(word_features_taste).encode('utf-8')))
 
 def document_features(document):
     document = document.replace(' ','|')
@@ -91,30 +93,35 @@ def document_features(document):
     return features
 
 
-
 f = open('./data/data250_1000.csv',encoding='utf8')
-reviewArr = []
+topWordsArr = []
 featuresets = []
 i = 0
 for line in f:
     if (i is numberOfRows):break
     i = i+1
-    id,review,n_taste, n_env,n_service = line.strip().split(',')
-    featuresets += [(document_features(review),n_service)] #TODO : EDIT
-    reviewArr.append(review)
+    id,topWords,n_taste, n_env,ans = line.strip().split(',')
+    featuresets += [(document_features(topWords),ans)] #TODO : EDIT
+    topWordsArr.append(topWords)
 
 
-print(featuresets)
+# print(featuresets)
+# o.write(featuresets)
 
-train_set, test_set = featuresets[:200], featuresets[200:]
+train_set, test_set = featuresets[:nRowTrain], featuresets[nRowTrain:]
 classifier = nltk.NaiveBayesClassifier.train(train_set)
 '''
 
-for i in reviewArr:
+for i in topWordsArr:
     print(i)
     print(classifier.classify(document_features(i)))
     print('-------')
 '''
+
 print('Service model, N = 2')
 print(nltk.classify.accuracy(classifier, test_set))
 print(classifier.show_most_informative_features(5))
+
+o.write('Service model, N = 2')
+o.write(str(nltk.classify.accuracy(classifier, test_set)))
+o.write(str(classifier.show_most_informative_features(5)))
