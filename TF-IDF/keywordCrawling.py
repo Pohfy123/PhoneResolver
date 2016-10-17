02ff445524a3b28490fac939e99f1ffcc915234a
Pohfy123 <pohfy123@gmail.com>
2016-10-17 21:35:54 +0700

    fetch description

--- TF-IDF/keywordCrawling.py
+++ TF-IDF/keywordCrawling.py
@@ -9,9 +9,13 @@ def fetchKeyword(urlArr):
         try: 
             html = urlopen(url).read()
             soup = BeautifulSoup(html,"lxml")
-            search = soup.findAll('meta',attrs={ 'name':'keywords'})
-            if len(search) > 0:
-                searchkey = searchkey+"\n"+str(search[0]['content'].encode('utf-8')) 
+            search_key = soup.findAll('meta',attrs={ 'name':'keywords'})
+            search_desc = soup.findAll('meta',attrs={ 'name':'description'})
+            if len(search_key) > 0:
+                searchkey = searchkey+"\n"+str(search_key[0]['content'].encode('utf-8'))
+            else:
+                if len(search_desc) > 0:
+                    searchkey = searchkey+"\n"+str(search_desc[0]['content'].encode('utf-8'))
         except IOError as e:
             print "I/O error({0}): {1}".format(e.errno, e.strerror)
         except:
