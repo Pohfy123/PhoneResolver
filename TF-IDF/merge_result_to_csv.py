5a5df56edf0f4254dbcd5dc83376cad916695c08
Pohfy123 <pohfy123@gmail.com>
2016-10-27 23:04:33 +0700

    fix merge to csv for multi class naive bayes

--- TF-IDF/merge_result_to_csv.py
+++ TF-IDF/merge_result_to_csv.py
@@ -7,17 +7,17 @@ import os
 def mergeResultToCSV(input_path, output_path, file_name="train.csv"):
     foutname = os.path.join(output_path, file_name)
     o = open(foutname, 'w')
-
+    number_dic = {}
     for dirpath, dirs, files in os.walk(input_path):
         # Root dir
         if dirpath == input_path:
-            print dirs
+            print "dirs",dirs
             continue
         deepestFolderName = os.path.split(dirpath)[-1]
         classId = deepestFolderName.split('_')[0]
         className = deepestFolderName.split('_')[1]
         print classId,'\t', className,'\t', dirpath
-        # Each Class folder
+        # Each Class folder 
         for f in files:
             fin_path = os.path.join(dirpath, f)
             fin_name = os.path.splitext(os.path.basename(fin_path))[0]
@@ -32,11 +32,28 @@ def mergeResultToCSV(input_path, output_path, file_name="train.csv"):
                     word, freq = line.decode('utf-8').strip().split(" - ")
                     # print freq
                     words.append(word.encode('utf-8'))
-                data_col1 = fin_name # Phone Number
-                data_col2 = ' '.join(words) # Bag of words
-                data_col3 = str(classId) # Result
-                o.write(data_col1+','+data_col2+','+data_col3)
-                o.write('\n')
+                number_dic[fin_name] = [' '.join(words),0,0]
+    for dirpath, dirs, files in os.walk(input_path):
+        # Root dir
+        if dirpath == input_path:
+            continue
+        deepestFolderName = os.path.split(dirpath)[-1]
+        classId = deepestFolderName.split('_')[0]
+        className = deepestFolderName.split('_')[1]
+        for f in files:
+            fin_path = os.path.join(dirpath, f)
+            fin_name = os.path.splitext(os.path.basename(fin_path))[0]
+            fin_ext = os.path.splitext(os.path.basename(fin_path))[1]
+            if fin_ext != '.txt':
+                continue
+            number_dic[fin_name][int(classId)] = 1
+    for num in number_dic:
+        data_col1 = num # Phone Number
+        data_col2 = number_dic[num][0] # Bag of words
+        data_col3 = str(number_dic[num][1]) # Result is_travel
+        data_col4 = str(number_dic[num][2]) # Result is_restaurant
+        o.write(data_col1+','+data_col2+','+data_col3+','+data_col4)
+        o.write('\n')
     o.close()
 
 path_train_data = './train-data/'
