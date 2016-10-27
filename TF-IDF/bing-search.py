10fca3ce144664958e2757d20b3b87121de13ff0
Roos <Vasvaroos@gmail.com>
2016-10-28 00:05:15 +0700

    add bing web search script # and add metadata

---
+++ TF-IDF/bing-search.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import os
+from py_bing_search import PyBingWebSearch
+
+# Init value
+bing_api_key = "exBWnPh+Htd8dkXYdW+WtNqDg42WAgzfUZAe9R6vMiU" # Enter your Bing API Key
+custom_params = "&Market='th-TH'"
+
+def searchBing(search_term):
+    bing_web = PyBingWebSearch(bing_api_key, search_term, web_only=True, custom_params=custom_params)
+    search_result = bing_web.search(limit=50, format='json')
+    return search_result
+
+def searchRelatedLinks(search_term_list, output_path, db_file_name="bing-search-result.csv"):
+    fileDBname = os.path.join(output_path, db_file_name)
+    oResult = open(fileDBname, 'w+')
+    
+    for search_term in search_term_list:
+        print(search_term)
+        fout_path = os.path.join(output_path, search_term+'.txt')
+
+        result = searchBing(search_term)
+
+        # save to each phone no file
+        with open(fout_path, 'w') as o:
+            urlList = [ result[i].url for i in range(len(result)) ] 
+            o.write('\n'.join(urlList))
+
+        # save to .csv database file
+        for link in result:
+            row = []
+            row.append(search_term) # Phone Number
+            row.append(link.title) # title
+            row.append(link.url) # url
+            row.append(link.description) # description
+            row.append(link.id) # id
+            row.append(link.meta.uri) # meta.uri
+            row.append(link.meta.type) # meta.type
+
+            oResult.write(','.join(row).encode('utf-8')) # print each row
+            oResult.write('\n')
+
+    oResult.close()
+
+
+def readPhoneNoList(path_input_file):
+    with open(path_number_list, "r") as fi:
+        nums = fi.read()
+        numArr = nums.split("\n")
+    return numArr
+
+
+path_number_list = "./number_input.txt" 
+path_url = "./temp-processing-data/00_url/"
+phoneNoList = readPhoneNoList(path_number_list)
+
+print('Use API Quota = ' + str(len(phoneNoList)) + ' Phone numbers')
+while True:
+    inputConfirm = raw_input('Are you sure? [Y/N]: ')
+    if(inputConfirm.upper()=='Y'):
+        searchRelatedLinks(phoneNoList, path_url)
+        break
+    elif(inputConfirm.upper()=='N'):
+        print 'Good Bye!'
+        break
