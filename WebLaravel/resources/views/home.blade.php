34be4751c7c617cddc381c13d05be79c978fdd4a
Pohfy123 <pohfy123@gmail.com>
2017-02-03 09:47:30 +0700

    remove footer from homepage

--- WebLaravel/resources/views/home.blade.php
+++ WebLaravel/resources/views/home.blade.php
@@ -501,61 +501,6 @@
         </div>
     </div>
 
-    <footer id="fh5co-footer" style="">
-        <div class="fh5co-overlay"></div>
-        <div class="fh5co-footer-content">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-3 col-sm-4 col-md-push-3">
-                        <h3 class="fh5co-lead">About</h3>
-                        <ul>
-                            <li><a href="#">Tour</a></li>
-                            <li><a href="#">Company</a></li>
-                            <li><a href="#">Jobs</a></li>
-                            <li><a href="#">Blog</a></li>
-                            <li><a href="#">New Features</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-3 col-sm-4 col-md-push-3">
-                        <h3 class="fh5co-lead">Support</h3>
-                        <ul>
-                            <li><a href="#">Help Center</a></li>
-                            <li><a href="#">Terms of Service</a></li>
-                            <li><a href="#">Security</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Careers</a></li>
-                            <li><a href="#">More Apps</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-3 col-sm-4 col-md-push-3">
-                        <h3 class="fh5co-lead">More Links</h3>
-                        <ul>
-                            <li><a href="#">Feedback</a></li>
-                            <li><a href="#">Frequently Ask Questions</a></li>
-                            <li><a href="#">Terms of Service</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Careers</a></li>
-                            <li><a href="#">More Apps</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col-md-3 col-sm-12 col-md-pull-9">
-                        <div class="fh5co-footer-logo"><a href="index.html">Outline</a></div>
-                        <p class="fh5co-copyright"><small>&copy; 2015. All Rights Reserved. <br>	by <a href="http://freehtml5.co/" target="_blank">FREEHTML5.co</a> Images: <a href="http://pexels.com/" target="_blank">Pexels</a></small></p>
-                        <p class="fh5co-social-icons">
-                            <a href="#"><i class="icon-twitter"></i></a>
-                            <a href="#"><i class="icon-facebook"></i></a>
-                            <a href="#"><i class="icon-instagram"></i></a>
-                            <a href="#"><i class="icon-dribbble"></i></a>
-                            <a href="#"><i class="icon-youtube"></i></a>
-                        </p>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </footer>
 
     @endsection
 
