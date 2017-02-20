e8d8fd2fc5bedd910359b7de653f9ae93f831fb1
Pohfy123 <pohfy123@gmail.com>
2017-02-20 14:47:51 +0700

    add auto tag

--- WebLaravel/public/js/result.js
+++ WebLaravel/public/js/result.js
@@ -14,21 +14,25 @@ $(document).on('click', '#btn-result', function() {
 
 $(document).on('click', '#try-url', function() {
     $('#input-demo-result').val('https://www.wongnai.com/restaurants/1667sR-%E0%B8%81%E0%B9%8B%E0%B8%A7%E0%B8%A2%E0%B8%88%E0%B8%B1%E0%B9%8A%E0%B8%9A%E0%B8%99%E0%B8%B2%E0%B8%A2%E0%B9%80%E0%B8%AD%E0%B9%87%E0%B8%81')
+    $('.input-tag').text("URL")
     resizeTextarea()
 })
 
 $(document).on('click', '#try-keyword', function() {
     $('#input-demo-result').val('ซอร์เทรล สาทร Bangkok')
+    $('.input-tag').text("KEYWORD")
     resizeTextarea()
 })
 
 $(document).on('click', '#try-phone', function() {
     $('#input-demo-result').val('02-690-1888')
+    $('.input-tag').text("PHONE-NO")
     resizeTextarea()
 })
 
 $(document).on('click', '#try-text', function() {
     $('#input-demo-result').val(`Krua Dok Mai Kao has regularly been packed out since it opened back in 2004, the brainchild of a savvy owner who saw the shortage of proper standalone restaurants in the Pathumwan neighborhood. Every day, office workers and families gather at the bright and airy two-story establishment, where bare concrete pillars stretch to the high ceilings amid a homey sprawl of marble tables, wooden seats and old-style armchairs. The menu has long been renowned for its international fare and bakery goods, which we feel masks the fact that the Thai offerings here lack authentic flavors. While popular dishes like the ham and cheese spring rolls (B105), baked baby clams with butter and garlic sauce served with French bread (B145) and spicy fried crispy cat fish with salted egg (B135) are all solid enough, others seem to be overloaded with sugar. The tom yam talay haeng (seafood with spicy sauce, B155) misses the point of the traditional recipe altogether, with the sweet notes totally overpowering the sour and spicy. It’s a similar story with the yam hed khem thong (spicy golden mushroom and shrimp salad, B95), which looks impressive but is all too saccharine and completely lacking in any tart lime, and the tom yum moo toon (spicy pork stew with herbs, B135/155), which is more a sweet soup than a spicy stew. That said, the kitchen is pretty quick and uses fresh ingredients, even if these aren’t always utilized to their full-flavored potential. In any case, the bakery is without a doubt the place’s strongest suit, serving a variety of delights where excessive sweetness is a good thing, like the tiramisu cake (B105) and Thai tea crepe cake (B105). If you’re stuck in this rather limited neighborhood for dining then Krua Dok Mai Kao isn’t a bad option, though limited parking and slightly slow service when they’re busy don’t exactly help. Just be prepared for a sugar rush whatever you decide to order. Corkage B300 (wine only). - See more at: http://bk.asia-city.com/restaurants/bangkok-restaurant-reviews/krua-dok-mai-kao#sthash.mwMtxT3B.dpuf`);
+    $('.input-tag').text("TEXT")
     resizeTextarea()
 })
 
@@ -67,9 +71,58 @@ function resizeTextarea() {
     // end auto resize textareaa
 }
 
+// auto tag
+
+function isPhoneNumber(inputtxt) {
+    var phoneno = /^\+?([0-9]{1,4})\)?[-. ]?([0-9\-]){6,}[0-9]|[0-9]{3,}?[-. ]?[0-9]$/;
+    if(phoneno.test(inputtxt)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function isUrl(inputtxt){
+    var urlPattern = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
+    if(urlPattern.test(inputtxt)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function autotag(inputtxt){
+    var MIN_LEN_TEXT = 50;
+    if(inputtxt.trim().length == 0) return "";
+    if(isUrl(inputtxt)) return "URL";
+    else if(isPhoneNumber(inputtxt)) return "PHONE-NO";
+    else if( inputtxt.length < MIN_LEN_TEXT )
+        return "KEYWORD"
+    else
+        return "TEXT"
+}
+$(document).on('input','#input-demo-first',function(){
+    var inputtxt = $('#input-demo-first').val()
+    var tag = autotag(inputtxt)
+    $('.input-tag').text(tag)
+})
+$(document).on('input','#input-demo-result',function(){
+    var inputtxt = $('#input-demo-result').val()
+    var tag = autotag(inputtxt)
+    $('.input-tag').text(tag)
+})
+// end auto tag
+
+
+var input;
+$(document).on('click','.btn-analyze1',function(){
+    input = $('#input-demo-first').val().trim
+})
+$(document).on('click','.btn-analyze2',function(){
+    input = $('#input-demo-result').val().trim
+})
 
 $(document).on('click','.btn-analyze',function(){
-    var input = $('.input-textarea').val()
     setTimeout(function(){}, 1000);
     $('.loading').removeClass('hide')
     $('.result').addClass('hide')
@@ -107,6 +160,17 @@ $(document).on('click','.btn-analyze',function(){
                 }
             };
             $('.json').html(library.json.prettyPrint(result));
+            $('#table-result').html(`<thead>
+                                                <tr>
+                                                    <th style="border-radius: 7px 0 0 0;">#</th>
+                                                    <th>Name</th>
+                                                    <th>Score</th>
+                                                    <th style="border-radius: 0 7px 0 0;">Confident ?</th>
+                                                </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr></tr>
+                                                </tbody>`)
             result.category.forEach(function (val, index, arr) {
                 if(parseFloat(val.score)>0.2){
                     $('#table-result tr:last').after(`<tr>
