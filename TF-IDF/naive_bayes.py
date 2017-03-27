42cebdf68aca9ae799ca795572e67590555d55b5
Vasvaroos Ngamdamrongkiat <Vasvaroos@gmail.com>
2017-03-27 20:11:50 +0700

    Change validation to example-based validation (wrong way)

--- TF-IDF/naive_bayes.py
+++ TF-IDF/naive_bayes.py
@@ -17,9 +17,9 @@ import sklearn.datasets
 
 from collections import Counter
 
-
-# result_labels = ['1_Air Travel Ticket Agencies','2_Home Stay','3_Hotel','4_Travel Bureaus','5_Airline Companies','6_Resorts & Bungalows','7_Seafood','8_Sukiyaki Shabu','9_Lounge Hotel Restaurant','10_Bakery Cake','11_Barbeque Grill','12_Coffee Shop','13_Ice Cream','14_Japanese'] ## Edit here
-result_labels = ['01_Airline','02_Accommodation','03_Tourism','04_Restaurant & Delivery','05_Sweet','06_Beverage'] ## Edit here
+NUMBER_OF_CLASSES = 4 # Edit here
+LABEL_NAMES = ['01_Airline','02_Accommodation','03_Tourism','04_Restaurant & Delivery'] ## Edit here
+train_data_file_name = './train-data/train_all_model.csv'
 
 def save_dict_words(words_list, filename_out='word_list.txt'):
     with open('./model/'+filename_out, "w") as outfile:
@@ -107,53 +107,79 @@ def display_confusion_matrix(classifier, test_data, showGraphic=False):
 
 
 def test_classifier(X_test, y_test, clf, test_size=0.4, y_names=None, confusion=False):
-            # train-test split
-            print 'test size is: %2.0f%%' % (test_size * 100)
-            y_predicted = clf.classify_many([fs for fs in X_test])
+    # train-test split
+    print 'test size is: %2.0f%%' % (test_size * 100)
+    y_predicted = clf.classify_many([fs for fs in X_test])
 
-            y_test = [int(y) for y in y_test]
-            y_predicted = [int(y) for y in y_predicted]
+    y_test = [int(y) for y in y_test]
+    y_predicted = [int(y) for y in y_predicted]
 
-            if not confusion:
-                print colored('Classification report:', 'magenta', attrs=['bold'])
-                print sklearn.metrics.classification_report(y_test, y_predicted, target_names=y_names)
-                print ">>>>> F1-SCORE : "+str(f1_score(y_test, y_predicted))+"  <<<<<<"
-                return f1_score(y_test, y_predicted)
-            else:
-                print colored('Confusion Matrix:', 'magenta', attrs=['bold'])
-                print sklearn.metrics.confusion_matrix(y_test, y_predicted)
+    if not confusion:
+        print colored('Classification report:', 'magenta', attrs=['bold'])
+        print sklearn.metrics.classification_report(y_test, y_predicted, target_names=y_names)
+        print ">>>>> F1-SCORE : "+str(f1_score(y_test, y_predicted))+"  <<<<<<"
+        return f1_score(y_test, y_predicted)
+    else:
+        print colored('Confusion Matrix:', 'magenta', attrs=['bold'])
+        print sklearn.metrics.confusion_matrix(y_test, y_predicted)
 
 
-def k_fold_evaluation(datasets, labels, nfolds=5):
-    # K-fold cross validation
-    # cv = cross_validation.KFold(len(datasets), n_folds=nfolds, shuffle=True, random_state=None)
-    print "Datasets |N|=%d" % len(datasets)
+def bin_list_to_dec(bin_list):
+    return int(''.join(map(str, bin_list)),2)
 
-    skf = StratifiedKFold(labels, n_folds=nfolds)
+
+def k_fold_evaluation(featuresets, labels, nfolds=5):
+    datasets = zip(featuresets, labels)
+    print "Datasets |N|=%d" % len(featuresets)
+
+    # Stratified K-fold cross validation
+    labels_dec = [bin_list_to_dec(y) for y in labels]
+    skf = StratifiedKFold(labels_dec, n_folds=nfolds)
 
     scores = []
     fold_idx = 1
     for train_index, test_index in skf:
+        print "> Evaluate a model: Fold#{}".format(fold_idx) 
+        start_time = time.time()
+
         train_data = [datasets[idx] for idx in train_index]
         test_data = [datasets[idx] for idx in test_index]
         print "Train data distribution: "
-        print Counter([x[1] for x in train_data ])
+        print Counter([bin_list_to_dec(x[1]) for x in train_data ])
         print "Test data distribution: "
-        print Counter([x[1] for x in test_data ])
+        print Counter([bin_list_to_dec(x[1]) for x in test_data ])
 
-        # Train model
-        classifier = train_model(train_data)
+        # Train models
+        classifiers = []
+        for class_id in range(NUMBER_OF_CLASSES):
+            this_class_labels = [label[class_id] for label in labels]
+            classifier = train_model(zip(featuresets, this_class_labels))
+            classifiers.append(classifier)
 
+        X_pred = [featuresets[idx] for idx in test_index]
+        y_pred = []
+        for classifier in classifiers:
+            y_pred.append(classifier.classify_many(X_pred))
+        y_pred = zip(*y_pred)
+        
         # Evaluate
-        X_test = [tc[0] for tc in test_data]
-        y_test = [tc[1] for tc in test_data]
-        score = test_classifier(X_test, y_test, classifier, test_size=0.2)
+        # X_test = [tc[0] for tc in test_data]
+        # y_test = [tc[1] for tc in test_data]
+        # score = test_classifier(X_test, y_test, classifier, test_size=0.2)
+
+        y_test = [labels[idx] for idx in test_index]
+        y_pred_dec = [bin_list_to_dec(y) for y in y_pred]
+        y_test_dec = [bin_list_to_dec(y) for y in y_test]
+        print sklearn.metrics.classification_report(y_test_dec, y_pred_dec)
+        score = f1_score(y_test_dec, y_pred_dec)
         print 'TEST#%d: F1-SCORE: %lf' % (fold_idx, score)
         
         # Display confusion matrix
         # display_confusion_matrix(classifier, test_data, showGraphic=False)
 
         scores.append(score)
+
+        print "--- Evaluate a model! ({} secs) ---".format(time.time() - start_time)
         fold_idx += 1
     print '##############'
     print 'TOTAL F1-SCORE: %lf' % (sum(scores)/len(scores)) 
@@ -177,102 +203,78 @@ def process_data(raw_datasets):
     datasets = [ (DictVec.inverse_transform(data['words'])[0], data['result']) for data in raw_datasets ]
     
     # Save DictVectorizer model & word list
-    save_model(DictVec, 'dictvect_'+('%02d'%file_id)+'.pickle')
-    save_dict_words(DictVec.get_feature_names(), 'wordlist_'+('%02d'%file_id)+'.txt')
+    save_model(DictVec, 'dictvect_'+('%02d'%class_id)+'.pickle')
+    save_dict_words(DictVec.get_feature_names(), 'wordlist_'+('%02d'%class_id)+'.txt')
 
     return datasets, DictVec
 
 
-def import_training_data(filename):
-    datasets = []
+def convert_to_sparse(featuresets):
+    # Convert word freq to Sparse matrix
+    v = DictVectorizer(sparse=True)
+    dict_vec = v.fit(featuresets)
+
+    # NOTICE: uncomment if sparse matrix is supported
+    # featuresets = dict_vec.transform(featuresets)
+    # featuresets = dict_vec.inverse_transform(x)
+
+    # Save DictVectorizer model & word list
+    save_model(dict_vec, 'dict_vect_models.pickle')
+    save_dict_words(dict_vec.get_feature_names(), 'word_lists.txt')
+
+    return featuresets
+
+
+def import_training_data(filename, n_class=1):
+    featuresets = []
+    labels = []
     with open(filename,'r') as fin:        
         for line in fin:
-            num,words,src_folder_id,result = line.split(",",3)
-            
+            phone_no, words, results = line.split(",", 2)
+
             # Skip empty file
-            if len(words)==0 or len(result)==0:
+            if len(words)==0 or len(results)==0:
                 continue
-                
-            phone_no = num.strip()
             
             words = dict([x.split(':') for x in words.strip().split(' ')])
             words = dict((k,1.0) for k,v in words.iteritems())
-            
-            result = result.strip()
-            
-            data = {
-                'phone_no' : phone_no, # no need
-                'words' : words,
-                'src_folder_id' : int(src_folder_id),
-                'result' : result
-            }
-            datasets.append(data)
-    return datasets
+
+            results = results.strip().split(",", n_class-1)
+
+            featuresets.append(words)
+            labels.append(results)
+    
+    return (convert_to_sparse(featuresets), labels)
 
 
 if __name__ == '__main__':
-    # Gather the filename of all training data files
-    filenames = []
-    for file in os.listdir("./train-data/"):
-        if file.endswith(".csv") and file.startswith('train-model'):
-            filenames.append(file)
-    filenames = [os.path.join('./train-data/', filename) for filename in filenames]
-
-    print 'All phone categories :\n', '\n'.join(result_labels) , '\n\n'
-    print 'All training data files :\n', '\n'.join(filenames) , '\n\n'
-    print "Start processing . . ."
-
+    print 'All phone categories :\n', '\n'.join(LABEL_NAMES) , '\n\n'
+    print "Start processing . . .\n\n"
     global_start_time = time.time()
 
-    for filename in filenames:    
-        # Initialization for each model    
-        file_id = int(filename[filename.index('train-model')+11: filename.index('.csv')])
-        print "========= TRAIN MODEL :: Category #%s (%s) =========" % (file_id,result_labels[file_id-1]) 
-        round_time = time.time()
-        
-        # Import datasets
-        start_time = time.time()
-        raw_datasets = import_training_data(filename)
-        print "> Imported data! (%f secs)" % (time.time() - start_time)
-            
-        # Preprocess data
-        start_time = time.time()
-        datasets, DictVec = process_data(raw_datasets)
-        print "> Preprocessed data! (%f secs)" % (time.time() - start_time)
+    # Import & Preprocess datasets
+    print "Start reading an input file . . ."
+    start_time = time.time()
+    featuresets, labels = import_training_data(train_data_file_name, NUMBER_OF_CLASSES)
+    print "> Imported & Preprocessed data! ({} secs)\n\n".format(time.time() - start_time)
 
-        # Train and save models
-        # start_time = time.time()
-        # classifier = train_model(datasets)
-        # save_model(classifier, "model_"+('%02d'%file_id)+".pickle")
-        # print "> Trained model! (%f secs)" % (time.time() - start_time)
+    # Train and save models
+    # print "Start reading an input file . . ."
+    # for class_id in range(NUMBER_OF_CLASSES):
+    #     print "> Training a model :: Category #{} {}".format(class_id+1,LABEL_NAMES[class_id]) 
+    #     start_time = time.time()
+    #     this_class_labels = [label[class_id] for label in labels]
+    #     classifier = train_model(zip(featuresets, this_class_labels))
+    #     save_model(classifier, "model_{:02d}.pickle".format(class_id+1))
+    #     print "--- Trained a model! ({} secs) ---".format(time.time() - start_time)
+    # print "\n"
 
-        # K-Fold Cross Validation (Accuracy test)
-        start_time = time.time()
-        labels = [row['src_folder_id'] for row in raw_datasets]
-        k_fold_evaluation(datasets, labels)
-        print "> Evaluation model! (%f secs)" % (time.time() - start_time)
-        print "--- %s seconds ---\n\n" % (time.time() - round_time)
+    # K-Fold Cross Validation (Accuracy test)
+    start_time = time.time()
+    k_fold_evaluation(featuresets, labels)
+    print "--- Evaluation model! {} seconds ---\n\n".format(time.time() - start_time)
 
     print "========================"
     print "Training Model SUCCESS!"
-    print "--- Total time: %s seconds ---" % (time.time() - global_start_time)
-
-
-    # Show result of each
-    # with open('./train-data/train.csv','r') as fin:
-    #     count = 0
-    #     for line in fin:
-    #         count += 1
-    #         if count >= number_of_train: 
-    #             num,words,is_travel,is_rest = line.split(",")
-    #             test_word = words.split(" ")
-    #             test_sent_features = {word: (word in test_word) for word in words_set}
-    #             is_rest = is_rest.replace("\n","")
-    #             dist = classifier.prob_classify(test_sent_features)
-    #             print num, "\tpred =",dist.max(),"\tans =", is_travel+""+is_rest, (dist.max()==is_travel+""+is_rest)
-
-                # diff = 0
-                # for label in dist.samples():
-                #     # print "\tlabel >>>",label," prob >>>" , dist.prob(label)
-                #     diff = abs(dist.prob(label)-diff)
-                # print "\tdiff",diff
+    print "--- Total time: {} seconds ---".format(time.time() - global_start_time)
+    print "========================"
